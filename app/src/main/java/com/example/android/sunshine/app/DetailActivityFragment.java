0dc38d7c8a716b1ca46341ec7a4c025278a19f0a
eiselems <marcus.eisele@gmail.com>
2015-12-17 16:13:34 +0100

    reached 'it works'

--- app/src/main/java/com/example/android/sunshine/app/DetailActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivityFragment.java
@@ -1,10 +1,14 @@
 package com.example.android.sunshine.app;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +21,16 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        Activity activity = getActivity();
+        if(activity.getIntent() != null && activity.getIntent().getExtras() != null){
+            String data = getActivity().getIntent().getExtras().getString(Intent.EXTRA_TEXT);
+            TextView textview = (TextView) rootView.findViewById(R.id.detail_text);
+            textview.setText(data);
+        }else{
+            Toast.makeText(getActivity(),"error reading data",Toast.LENGTH_SHORT).show();
+        }
+        return rootView;
     }
 }
